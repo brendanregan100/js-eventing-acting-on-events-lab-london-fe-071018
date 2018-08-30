e3a4f841ab8c4d9308d5e6ed5cf00a45c64ec558
Brendan regan <brendanregan100@gmail.com>
2018-08-30 17:21:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
- 
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`
-  }
-}
+const dodger = document.getElementById('dodger')
 
-document.addEventListener('keydown', function(e) {
-  if (e.which === 39) {
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+  dodger.style.left = `${left + 1}px`
+  }
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+  dodger.style.left = `${left - 1}px`
+  } 
+
+function moveDodgerDown() {
+  var bottomNumbers = dodger.style.bottom.replace('px', '')
+  var bottom = parseInt(bottomNumbers, 10)
+  dodger.style.bottom = `${bottom - 1}px`
+  }
+  
+function moveDodgerUp() {
+  var bottomNumbers = dodger.style.bottom.replace('px', '')
+  var bottom = parseInt(bottomNumbers, 10)
+  dodger.style.bottom = `${bottom + 1}px`
+  }
+
+document.addEventListener('keydown', function(e){
+  if (+e.which === 37) {
+    moveDodgerLeft()
+  }
+  if (+e.which === 38) {
+    moveDodgerUp()
+  }
+  if (+e.which === 39) {
     moveDodgerRight()
+  }
+  if (+e.which === 40) {
+    moveDodgerDown()
   }
 })
